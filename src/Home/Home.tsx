a838bc7f18b78205eae14b3f6964ed55633bf8fe
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-11 06:37:16 +0600

    created the basic navbar

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
     return (
         <div>
+            <Navbar />
             <h5>This is Home page</h5>            
         </div>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,8 @@ import { router } from './Routes/Routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <div className='max-w-screen-2xl mx-auto'>
     <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
