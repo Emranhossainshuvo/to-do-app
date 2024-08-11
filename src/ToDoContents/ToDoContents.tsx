cd80ca7d901bdf8de08c114c4617a5d860c90160
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-11 11:11:33 +0600

    created cart in the disply contents page to show the cards of to do

--- src/ToDoContents/ToDoContents.tsx
+++ src/ToDoContents/ToDoContents.tsx
@@ -1,9 +1,17 @@
+import ToDoCard from "./ToDoCard";
 
 const ToDoContents = () => {
     return (
-        <div>
-          <h4>To do contents</h4>  
-        </div>
+        <>
+            {/* this div will hold everything in this component */}
+            <div>
+                <div className="bg-[#ecedee]">
+                    <li className="marker:text-[#5030e5] text-center mb-5">To do <span className="bg-[#e0e0e0] px-2 py-1  rounded-[50px]">3</span></li>
+                    <hr className="h-1 bg-[#5030e5]" />
+                    <ToDoCard badge={'red'} description={'Brainstorming brings team members diverse experience into play.'} title={'Brainstorming'} deadline={'12/5/24'} />
+                </div>
+            </div>
+        </>
     );
 };
 
