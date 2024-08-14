6d2e5c85fe0f1f6cedd0db4766f9a36594d7f5b1
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-14 08:40:09 +0600

    reinitialized the project

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,19 +1,8 @@
-import Navbar from "../components/Navbar/Navbar";
-import Sidebar from "../components/Sidebar/Sidebar";
-import ToDoContents from "../ToDoContents/ToDoContents";
 
 const Home = () => {
     return (
         <div>
-            <Navbar />
-            <div className="w-full flex">
-                <div className="w-[16%]">
-                    <Sidebar />
-                </div>
-                <div className="w-full">
-                    <ToDoContents />
-                </div>
-            </div>
+            <h2>This is title</h2>
         </div>
     );
 };
